2b2e25c22f5af0bdaadead39244a126203a999ad
Benedict Omare <benedict.omare@un.org>
2025-05-23 13:41:22 +0200

    remove report extra

--- www/sparql.php
+++ www/sparql.php
@@ -107,18 +107,39 @@
       To use the tool: input a query or edit the sample query, set any options and press "Run Query"
       
       You can find the IPBES Ontology here: https://ontology.ipbes.net/report/report.ttl.
+
+      Examples
+      # Knowledge gaps: Specific
+        PREFIX ipbes: <http://ontology.ipbes.net/report>
+        SELECT ?gap ?subchapter ?chapter ?description
+        WHERE {
+          ?gap a ipbes:KnowledgeGap .
+          OPTIONAL { ?gap ipbes:SubChapter ?subchapter }
+          OPTIONAL { ?gap ipbes:hasDescription ?description }
+        }
+        ORDER BY ?gap
+
+      # Knowledge gaps: Query all graphs: Make sure Select Target Graph is empty (i.e. no graph should be selected)
+      PREFIX ipbes: <http://ontology.ipbes.net/report>
+      SELECT ?g ?gap ?subchapter ?chapter ?description
+      WHERE {
+        GRAPH ?g {
+          ?gap a ipbes:KnowledgeGap .
+          OPTIONAL { ?gap ipbes:SubChapter ?subchapter }
+          OPTIONAL { ?gap ipbes:hasDescription ?description }
+        }
+      }
+      ORDER BY ?gap
     </p>
 
     <label for="query">SPARQL Query:</label>
     <textarea id="query">
-        PREFIX ipbes: &lt;http://ontology.ipbes.net/report&gt;
-        SELECT ?g ?gap ?subchapter ?chapter ?description
+        PREFIX ipbes: <http://ontology.ipbes.net/report>
+        SELECT ?gap ?subchapter ?chapter ?description
         WHERE {
-        GRAPH ?g {
-            ?gap a ipbes:KnowledgeGap .
-            OPTIONAL { ?gap ipbes:SubChapter ?subchapter }
-            OPTIONAL { ?gap ipbes:hasDescription ?description }
-        }
+          ?gap a ipbes:KnowledgeGap .
+          OPTIONAL { ?gap ipbes:SubChapter ?subchapter }
+          OPTIONAL { ?gap ipbes:hasDescription ?description }
         }
         ORDER BY ?gap
     </textarea>
