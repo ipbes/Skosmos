8caf145300d00987632d8440fffa75b744e6f097
Benedict Omare <benedict.omare@un.org>
2025-05-23 13:46:30 +0200

    remove examples

--- www/sparql.php
+++ www/sparql.php
@@ -108,28 +108,8 @@
       
       You can find the IPBES Ontology here: https://ontology.ipbes.net/report/report.ttl.
 
-      Examples
-      # Knowledge gaps: Specific
-        PREFIX ipbes: <http://ontology.ipbes.net/report>
-        SELECT ?gap ?subchapter ?chapter ?description
-        WHERE {
-          ?gap a ipbes:KnowledgeGap .
-          OPTIONAL { ?gap ipbes:SubChapter ?subchapter }
-          OPTIONAL { ?gap ipbes:hasDescription ?description }
-        }
-        ORDER BY ?gap
-
-      # Knowledge gaps: Query all graphs: Make sure Select Target Graph is empty (i.e. no graph should be selected)
-      PREFIX ipbes: <http://ontology.ipbes.net/report>
-      SELECT ?g ?gap ?subchapter ?chapter ?description
-      WHERE {
-        GRAPH ?g {
-          ?gap a ipbes:KnowledgeGap .
-          OPTIONAL { ?gap ipbes:SubChapter ?subchapter }
-          OPTIONAL { ?gap ipbes:hasDescription ?description }
-        }
-      }
-      ORDER BY ?gap
+      Examples: See here
+      
     </p>
 
     <label for="query">SPARQL Query:</label>
